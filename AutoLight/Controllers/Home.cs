e715495e7ce41b7bffa09f26f04b44f60284aa2b
Job Rapati <jrapati@hotmail.nl>
2021-09-28 16:37:21 +0200

    Allow light switching based on weather using OpenWeatherMap

--- AutoLight/Controllers/Home.cs
+++ AutoLight/Controllers/Home.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AutoLight.Models;
 using Hangfire;
@@ -18,13 +19,18 @@ namespace AutoLight.Controllers
     [Route("[controller]/[action]")]
     public class HomeController : ControllerBase
     {
-
+        private string[] groupToMatch = { "2", "3", "5", "6", "7"};
+        // private string[] codesToMatch = { "804" };
         private readonly ILogger<HomeController> _logger;
         private List<string> Jobs = new List<string>();
         
         //Home Assistant Token
         private const string token =
             "";
+
+        //OpenWeathMap API key & City
+        private const string weatherApiKey = "";
+        private const string city = "";
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -33,6 +39,13 @@ namespace AutoLight.Controllers
 
         [HttpGet]
         public async Task<string> Get()
+        {
+            await SwitchLightJob("on");
+            CreateJob();
+            return $"Working on it";
+        }
+
+        private async void CreateJob()
         {
             var sunsetRoute = "https://api.sunrise-sunset.org/json?lat=51.5562404&lng=5.0886014";
             var client = new HttpClient();
@@ -45,34 +58,43 @@ namespace AutoLight.Controllers
             if (DateTime.Now < sunrise)
             {
                 _logger.LogInformation("Scheduling light to turn off at sunrise");
-                SwitchLightJob("on");
+                await SwitchLightJob("on");
                 var job = BackgroundJob.Schedule(() => SwitchLightJob("off"), sunrise);
                 WriteToFile(job);
             }
             else if (DateTime.Now > sunrise && DateTime.Now < sunset)
             {
                 _logger.LogInformation("Scheduling light to turn on at sunset");
-                SwitchLightJob("off");
+                await SwitchLightJob("off");
                 var job = BackgroundJob.Schedule(() => SwitchLightJob("on"), sunset.AddHours(-1));
                 _logger.LogInformation("Job ID: {0}", job);
                 WriteToFile(job);
+                
+                RecurringJob.AddOrUpdate("WeatherCheck", () => CheckWeather(), Cron.Minutely);
             }
             else if (DateTime.Now > sunset)
             {
                 _logger.LogInformation("Scheduling light to turn off at sunrise");
-                SwitchLightJob("on");
+                await SwitchLightJob("on");
                 var secondTimeResult = await client.GetAsync($"{sunsetRoute}&date={DateTime.Now.Year}-{DateTime.Now.Month}-{DateTime.Now.Day}");
                 var nextDay = JsonConvert.DeserializeObject<SunriseSunset>(await secondTimeResult.Content.ReadAsStringAsync());
                 var job = BackgroundJob.Schedule(() => SwitchLightJob("off"), DateTime.Parse(nextDay.Results.Sunrise).ToLocalTime());
                 WriteToFile(job);
             }
-            return $"Working on it";
         }
 
         [HttpGet]
         public async Task<string> TurnOff()
         {
             var result = await SwitchLightJob("off");
+            ClearJobs();
+
+            return $"{result.StatusCode}: {await result.Content.ReadAsStringAsync()}";
+        }
+
+        private void ClearJobs()
+        {
+            RecurringJob.RemoveIfExists("WeatherCheck");
             if (System.IO.File.Exists("jobs.txt"))
             {
                 using (StreamReader r = new StreamReader("jobs.txt"))
@@ -88,8 +110,6 @@ namespace AutoLight.Controllers
                 }
                 System.IO.File.Delete("jobs.txt");
             }
-
-            return $"{result.StatusCode}: {await result.Content.ReadAsStringAsync()}";
         }
 
         [ApiExplorerSettings(IgnoreApi = true)]
@@ -111,6 +131,27 @@ namespace AutoLight.Controllers
                 w.Write($"{jobId},");
                 w.Dispose();
             }
+        }
+        [HttpGet]
+        public async Task CheckWeather()
+        {
+            bool turnOff = true;
+            var client = new HttpClient();
+            var result =
+                await client.GetAsync($"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weatherApiKey}");
+            var weatherResult = JsonConvert.DeserializeObject<WeatherResult>(await result.Content.ReadAsStringAsync());
+            foreach (var group in groupToMatch)
+            {
+                if (weatherResult.Weathers[0].Id.ToString().StartsWith(group))
+                {
+                    await SwitchLightJob("on");
+                    turnOff = false;
+                    break;
+                }
+            }
+
+            if (turnOff)
+                await SwitchLightJob("off");
         }
     }
 }
---
+++ AutoLight/Models/WeatherResult.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace AutoLight.Models
+{
+    public struct Coords
+    {
+        public float Lon { get; set; }
+        public float Lan { get; set; }
+    }
+
+    public struct Weather
+    {
+        public uint Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+
+    public struct Main
+    {
+        public float Temp { get; set; }
+        [JsonProperty("feels_like")]
+        public float FeelsLike { get; set; }
+        [JsonProperty("temp_min")]
+        public float TempMin { get; set; }
+        [JsonProperty("temp_max")]
+        public float TempMax { get; set; }
+        public float Pressure { get; set; }
+        public float Humidity { get; set; }
+    }
+
+    public struct Wind
+    {
+        public double Speed { get; set; }
+        public int Deg { get; set; }
+        public double Gust { get; set; }
+    }
+
+    public struct Clouds
+    {
+        public int All { get; set; }
+    }
+
+    public struct Sys
+    {
+        public int Type { get; set; }
+        public uint Id { get; set; }
+        public string Country { get; set; }
+        public uint Sunrise { get; set; }
+        public uint Sunset { get; set; }
+    }
+    
+    public class WeatherResult
+    {
+        public Coords Coords { get; set; }
+        [JsonProperty("weather")]
+        public List<Weather> Weathers { get; set; }
+        public string Base { get; set; }
+        public Main Main { get; set; }
+        public float Visibilty { get; set; }
+        public Wind Wind { get; set; }
+        public Clouds Clouds { get; set; }
+        public uint Dt { get; set; }
+        public Sys Sys { get; set; }
+        public uint Timezone { get; set; }
+        public uint Id { get; set; }
+        public string Name { get; set; }
+        public uint Cod { get; set; }
+    }
+}
